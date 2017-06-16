7cbfadb06fd6245716cd4341827dcd456a641a42
yangcx <charlesyoung0811@gmail.com>
2017-06-16 10:31:50 +0800

    上次提交信息错误：数据库中保存salt值处理：修改对应的SaltStyle为"COLUMN"及authenticationQuery【CAUTION：Shiro默认使用shiro 默认使用org.apache.commons.beanutils.BeanUtils不支持Enum转换，需要自行处理 不进行 Enum 转换，需要自行注册】

--- ch5/src/main/test/PasswordTest.java
+++ ch5/src/main/test/PasswordTest.java
@@ -1,8 +1,11 @@
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.beanutils.converters.AbstractConverter;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.junit.After;
@@ -21,6 +24,13 @@ public class PasswordTest {
     private Subject subject;
 
     @Test
+    public void testJdbcHashCredentialsMatcher(){
+        //cxy shiro 默认使用org.apache.commons.beanutils.BeanUtils不支持Enum转换，需要自行处理
+        BeanUtilsBean.getInstance().getConvertUtils().register(new EnumConverter(), JdbcRealm.SaltStyle.class);
+        login("shiro-jdbc-hashedCredentialsMatcher.ini", "liu", "123");
+    }
+
+    @Test
     public void testHashedCredentialsMatcherWithMyRealm2() {
         //使用testGeneratePassword生成的散列密码
         login("shiro-hashedCredentialsMatcher.ini", "liu", "123");
@@ -34,6 +44,17 @@ public class PasswordTest {
     @Test
     public void testPasswordServiceWithJdbcRealm() {
         login("shiro-jdbc-passwordservice.ini", "wu", "123");
+    }
+
+    private class EnumConverter extends AbstractConverter{
+
+        protected Object convertToType(Class aClass, Object o) throws Throwable {
+            return Enum.valueOf(aClass,o.toString());
+        }
+
+        protected Class getDefaultType() {
+            return null;
+        }
     }
 
     public void login(String configFile, String username, String password) {
