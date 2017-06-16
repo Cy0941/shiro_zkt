7f77f8b27b1ba03dc920ec55812b550e7ea3eb90
yangcx <charlesyoung0811@gmail.com>
2017-06-16 18:29:06 +0800

    shiro简单web集成【非spring环境】

---
+++ ch7/src/main/java/cn/cxy/shiro/servlet/LoginServlet.java
@@ -0,0 +1,58 @@
+package cn.cxy.shiro.servlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Function: TODO
+ * Reason: TODO ADD REASON(可选).</br>
+ * Date: 2017/6/16 17:42 </br>
+ *
+ * @author: cx.yang
+ * @since: Thinkingbar Web Project 1.0
+ */
+
+@WebServlet(value = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String error = null;
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
+            subject.login(token);
+        } catch (UnknownAccountException e) {
+            error = "用户名/密码错误";
+        } catch (IncorrectCredentialsException e) {
+            error = "用户名/密码错误";
+        } catch (AuthenticationException e) {
+            error = "其他错误：" + e.getMessage();
+        }
+        if (null != error){
+            req.setAttribute("error",error);
+            req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req,resp);
+        }else {
+            req.getRequestDispatcher("/WEB-INF/jsp/loginSuccess.jsp").forward(req,resp);
+        }
+    }
+}
